dda499166f2774452b0261f6fc07eb08a4022a9f
Krishna Prasad Kuchur <krishnakuchur4@gmail.com>
2017-11-03 22:34:05 -0700

    Solved problem 136

--- src/LeetCodeEasy/SingleNumber136.java
+++ src/LeetCodeEasy/SingleNumber136.java
@@ -1,8 +1,51 @@
 package LeetCodeEasy;
 
+//Problem 136: Given an array of integers, every element appears twice except for one. Find that single one.
+//
+//        Note:
+//        Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+import java.util.HashMap;
+
 public class SingleNumber136 {
 
     public static void main(String arggs[]){
-        System.out.println("hi");
+        int[] nums = {0,0,1};
+        System.out.println("The single occuring number using hash is: "+singleNumberWithExtraSpace(nums));
+        System.out.println("The single occuring number using xor is: "+singleNumber(nums));
+    }
+
+
+    public static int singleNumber(int[] nums){
+        int length = nums.length;
+
+        int xor = 0;
+        for(int i=0; i<length; i++){
+            xor ^= nums[i];
+        }
+        return xor;
+    }
+
+
+
+    public static int singleNumberWithExtraSpace(int[] nums) {
+        HashMap<Integer, Integer> countHash = new HashMap<>();
+        int length = nums.length;
+        int ans = 0;
+
+        for(int i=0; i<length; i++){
+            if (countHash.containsKey(nums[i])){
+                countHash.remove(nums[i]);
+            }
+            else {
+                countHash.put(nums[i],1);
+            }
+        }
+
+        for (Integer num : countHash.keySet()) {
+            ans =  num;
+        }
+        return ans;
+
     }
 }
